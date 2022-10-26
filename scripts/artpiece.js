68fdfecb8446d3657482639e48b4e9f7a8cfd1fc
Matthew Sidaway <matthewsidaway@outlook.com>
2022-10-26 22:51:58 +0100

    moved scripts to own directory

---
+++ scripts/artpiece.js
@@ -0,0 +1,11 @@
+export class ArtPiece {
+  constructor(author, title, medium, dimensions, year, image, moreInfoLink) {
+    this.author = author;
+    this.title = title;
+    this.medium = medium;
+    this.dimensions = dimensions;
+    this.year = year;
+    this.image = image;
+    this.moreInfoLink = moreInfoLink;
+  }
+}
