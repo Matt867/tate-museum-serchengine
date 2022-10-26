0b8ab59c309c1ebdca419f64cb1b9ac93677abfd
Lorena Gil <lorenagil@fb.com>
2022-10-26 17:06:20 +0100

    Create function.js and update artpiece class.

---
+++ functions.js
@@ -0,0 +1,25 @@
+const ArtPiece = require('./artpiece');
+
+function photoLink(record) {
+  const idPhoto = record.fields.thumbnailurl.filename;
+  const firstLetteridPhoto = idPhoto[0];
+  const first3LettersidPhoto = idPhoto.substring(0, 3);
+
+  const link = `https://media.tate.org.uk/art/images/work/${firstLetteridPhoto}/${first3LettersidPhoto}/${idPhoto}`
+
+  return link
+}
+
+function createArtPiece(record) {
+  const author = record.fields.artist;
+  const title = record.fields.title;
+  const medium = record.fields.medium;
+  const dimmensions =`${record.fields.width} x ${record.fields.height} ${record.fields.units}`;
+  const year = record.fields.year;
+  const image = photoLink(record);
+  const moreInfoLink = record.fields.url;
+
+  const art = new ArtPiece(author, title, medium, dimmensions, year, image, moreInfoLink);
+
+  return art;
+}
